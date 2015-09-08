0c28bc64f890dd9bc6f213be05e3b976b4dc7b7a
codeRepJamie <jamiextsang@gmail.com>
2015-09-09 01:50:00 +0800

    推荐分类一半

--- scopeJson.js
+++ scopeJson.js
@@ -1,19 +1,182 @@
-﻿var dataScope=angular.module('tmDataScope',[]);
-dataScope.factory('sData',function(){
-    var root={};
-    
-    root.headerStyle={"hot_search_repeat":{"config":{"title":"热搜","node_type":"elemtary-reapeat","controller":"A_1"},"model":[{"value":"","$$hashKey":"object:7","text":"签证","url":"https://cgzl.alitrip.com/category-905437805.htm?spm=a1z10.4-b.w5001-10132340897.3.gifmzZ&search=y&catName=%C8%AB%C7%F2%C7%A9%D6%A4&scene=taobao_shop"},{"value":"","$$hashKey":"object:12","text":"日本","url":"https://cgzl.alitrip.com/category-905439019.htm?spm=a1z10.4-b.w5001-10132340897.4.gifmzZ&search=y&catName=%C8%D5%B1%BE&scene=taobao_shop"},{"url":"https://cgzl.alitrip.com/category-957226595.htm?spm=a1z10.5-b.w5001-10132340897.5.o5G28W&search=y&catName=%CC%A8%CD%E5&scene=taobao_shop","text":"台湾","$$hashKey":"object:5"},{"value":"","$$hashKey":"object:35","text":"门票","url":"https://cgzl.alitrip.com/shop/view_shop.htm?spm=a1z10.5-b.w5001-10132340897.6.jjRh6G&q=%C3%C5%C6%B1&type=p&from=inshophq_6_0&search=y&newHeader_b=s_from&searcy_type=item&scene=taobao_shop"}]},"menu_repeat":{"model":[{"value":"","$$hashKey":"object:7","text":"HOME首页","url":"//cgzl.tmall.com/?spm=a1z10.1-b.w5001-10132340897.7.8INIoX&amp;scene=taobao_shop/"},{"value":"","$$hashKey":"object:12","text":"全球签证","url":"https://cgzl.alitrip.com/category-905437805.htm?spm=a1z10.1-b.w5001-10132340897.8.8INIoX&scene=taobao_shop"},{"value":"","$$hashKey":"object:13","text":"欧洲","url":"https://cgzl.alitrip.com/category-981688017.htm?spm=a1z10.5-b.w5001-10132340897.9.YlIJIB&search=y&scene=taobao_shop#TmshopSrchNav"},{"value":"","$$hashKey":"object:15","text":"省内海滩","url":"https://cgzl.alitrip.com/category-927118466.htm?spm=a1z10.5-b.w5001-10132340897.10.Bbe9Rc&search=y&parentCatId=927118465&parentCatName=%B9%E3%B6%AB%D6%DC%B1%DF%D3%CE&catName=%C9%B3%CC%B2%2F%BA%A3%B5%BA&scene=taobao_shop"},{"value":"","$$hashKey":"object:11","text":"东南亚","url":"https://cgzl.alitrip.com/category-1069648108.htm?spm=a1z10.5-b.w5001-10132340897.11.pS5RPk&search=y&parentCatId=1006897217&parentCatName=%B3%F6%BE%B3%D7%D4%D3%C9%D0%D0%2F%B8%FA%CD%C5%D3%CE&catName=%B6%AB%C4%CF%D1%C7&scene=taobao_shop"},{"url":"https://cgzl.alitrip.com/category-906673429.htm?spm=a1z10.5-b.w5001-10132340897.12.5oXpYP&search=y&catName=%CF%E3%B8%DB&scene=taobao_shop","text":"香港","$$hashKey":"object:5"},{"value":"","$$hashKey":"object:59","text":"日本","url":"https://cgzl.alitrip.com/category-905439019.htm?spm=a1z10.5-b.w5001-10132340897.13.LJEr6Z&search=y&scene=taobao_shop#TmshopSrchNav"},{"value":"","$$hashKey":"object:63","text":"长隆","url":"https://cgzl.alitrip.com/category-984690522.htm?spm=a1z10.5-b.w5001-10132340897.14.u3mA3i&search=y&scene=taobao_shop#TmshopSrchNav"},{"value":"","$$hashKey":"object:69","text":"周边游","url":"https://cgzl.alitrip.com/category-945317391.htm?spm=a1z10.5-b.w5001-10132340897.15.hliODb&search=y&parentCatId=927118465&parentCatName=%B9%E3%B6%AB%D6%DC%B1%DF%D3%CE&catName=%B8%FA%CD%C5%D3%CE&scene=taobao_shop"},{"value":"","$$hashKey":"object:73","text":"关于我们","url":"https://cgzl.alitrip.com/p/rd504540.htm?spm=a1z10.5-b.w5001-10132340897.16.RML9mT&scene=taobao_shop"}],"config":{"title":"导航栏","node-type":"elemtary-reapeat","max":10,"controller":"A_2"}},"config":{"title":"店铺招牌","node_type":"parent","data_num":0}};
-    
-    
-    root.banner={"banner_pic_repeat":{"model":[{"value":"","$$hashKey":"object:25","pic":"https://img.alicdn.com/imgextra/i2/2067413496/TB2OTSIeVXXXXcfXXXXXXXXXXXX_!!2067413496.jpg","url":"http://cgzl.tmall.com/p/rd743438.htm"},{"value":"","$$hashKey":"object:7","pic":"https://img.alicdn.com/imgextra/i1/2067413496/TB2ikBPdVXXXXXpXXXXXXXXXXXX_!!2067413496.jpg","url":"http://cgzl.tmall.com/p/rd600857.htm"},{"value":"","$$hashKey":"object:55","pic":"https://img.alicdn.com/imgextra/i2/2067413496/TB2ZzY1dVXXXXcgXXXXXXXXXXXX_!!2067413496.jpg","url":"http://cgzl.tmall.com/p/rd075928.htm"},{"value":"","$$hashKey":"object:54","pic":"http://img.alicdn.com/imgextra/i2/2067413496/TB2RbfrdFXXXXXtXXXXXXXXXXXX_!!2067413496.jpg","url":"http://cgzl.alitrip.com/p/rd873746.htm?spm=a1z10.1-b.w5003-9882021204.1.W1lMRr&scene=taobao_shop"},{"value":"","$$hashKey":"object:61","pic":"http://img03.taobaocdn.com/imgextra/i3/2067413496/TB2LTBrbVXXXXatXpXXXXXXXXXX_!!2067413496.jpg","url":"http://cgzl.tmall.com/p/visa.htm"}],"config":{"title":"焦点图详情","node-type":"elemtary-reapeat","controller":"B_1","max":7,"picSize-x":"140","picSize-y":"31"}},"config":{"title":"焦点图","node-type":"parent","data_num":1}};
-    
-    
-    root.callcenter={"travel_repeat":{"config":{"title":"旅游度假","node-type":"elemtary-reapeat","max":10,"controller":"A_3"},"model":[{"value":"","$$hashKey":"object:7","text":"坤坤"},{"value":"","$$hashKey":"object:44","text":"大吉"},{"value":"","$$hashKey":"object:49","text":"岚岚"},{"value":"","$$hashKey":"object:53","text":"大斌"},{"value":"","$$hashKey":"object:58","text":"梵梵"}]},"ticket_repeat":{"config":{"title":"订房订票","node-type":"elemtary-reapeat","max":10,"controller":"A_3"},"model":[{"value":"","$$hashKey":"object:7","text":"云儿"},{"value":"","$$hashKey":"object:63","text":"珊珊"},{"value":"","$$hashKey":"object:68","text":"斋斋"}]},"config":{"title":"旺旺客服","node-type":"parent","data_num":2}};
-    
+﻿var dataScope = angular.module('tmDataScope', []);
+dataScope.factory('sData', function () {
+    var root = {};
+
+    root.headerStyle = {
+        "hot_search_repeat": {
+            "config": {
+                "title": "热搜",
+                "node_type": "elemtary-reapeat",
+                "controller": "A_1"
+            },
+            "model": [{
+                "value": "",
+                "$$hashKey": "object:7",
+                "text": "签证",
+                "url": "https://cgzl.alitrip.com/category-905437805.htm?spm=a1z10.4-b.w5001-10132340897.3.gifmzZ&search=y&catName=%C8%AB%C7%F2%C7%A9%D6%A4&scene=taobao_shop"
+            }, {
+                "value": "",
+                "$$hashKey": "object:12",
+                "text": "日本",
+                "url": "https://cgzl.alitrip.com/category-905439019.htm?spm=a1z10.4-b.w5001-10132340897.4.gifmzZ&search=y&catName=%C8%D5%B1%BE&scene=taobao_shop"
+            }, {
+                "url": "https://cgzl.alitrip.com/category-957226595.htm?spm=a1z10.5-b.w5001-10132340897.5.o5G28W&search=y&catName=%CC%A8%CD%E5&scene=taobao_shop",
+                "text": "台湾",
+                "$$hashKey": "object:5"
+            }, {
+                "value": "",
+                "$$hashKey": "object:35",
+                "text": "门票",
+                "url": "https://cgzl.alitrip.com/shop/view_shop.htm?spm=a1z10.5-b.w5001-10132340897.6.jjRh6G&q=%C3%C5%C6%B1&type=p&from=inshophq_6_0&search=y&newHeader_b=s_from&searcy_type=item&scene=taobao_shop"
+            }]
+        },
+        "menu_repeat": {
+            "model": [{
+                "value": "",
+                "$$hashKey": "object:7",
+                "text": "HOME首页",
+                "url": "//cgzl.tmall.com/?spm=a1z10.1-b.w5001-10132340897.7.8INIoX&amp;scene=taobao_shop/"
+            }, {
+                "value": "",
+                "$$hashKey": "object:12",
+                "text": "全球签证",
+                "url": "https://cgzl.alitrip.com/category-905437805.htm?spm=a1z10.1-b.w5001-10132340897.8.8INIoX&scene=taobao_shop"
+            }, {
+                "value": "",
+                "$$hashKey": "object:13",
+                "text": "欧洲",
+                "url": "https://cgzl.alitrip.com/category-981688017.htm?spm=a1z10.5-b.w5001-10132340897.9.YlIJIB&search=y&scene=taobao_shop#TmshopSrchNav"
+            }, {
+                "value": "",
+                "$$hashKey": "object:15",
+                "text": "省内海滩",
+                "url": "https://cgzl.alitrip.com/category-927118466.htm?spm=a1z10.5-b.w5001-10132340897.10.Bbe9Rc&search=y&parentCatId=927118465&parentCatName=%B9%E3%B6%AB%D6%DC%B1%DF%D3%CE&catName=%C9%B3%CC%B2%2F%BA%A3%B5%BA&scene=taobao_shop"
+            }, {
+                "value": "",
+                "$$hashKey": "object:11",
+                "text": "东南亚",
+                "url": "https://cgzl.alitrip.com/category-1069648108.htm?spm=a1z10.5-b.w5001-10132340897.11.pS5RPk&search=y&parentCatId=1006897217&parentCatName=%B3%F6%BE%B3%D7%D4%D3%C9%D0%D0%2F%B8%FA%CD%C5%D3%CE&catName=%B6%AB%C4%CF%D1%C7&scene=taobao_shop"
+            }, {
+                "url": "https://cgzl.alitrip.com/category-906673429.htm?spm=a1z10.5-b.w5001-10132340897.12.5oXpYP&search=y&catName=%CF%E3%B8%DB&scene=taobao_shop",
+                "text": "香港",
+                "$$hashKey": "object:5"
+            }, {
+                "value": "",
+                "$$hashKey": "object:59",
+                "text": "日本",
+                "url": "https://cgzl.alitrip.com/category-905439019.htm?spm=a1z10.5-b.w5001-10132340897.13.LJEr6Z&search=y&scene=taobao_shop#TmshopSrchNav"
+            }, {
+                "value": "",
+                "$$hashKey": "object:63",
+                "text": "长隆",
+                "url": "https://cgzl.alitrip.com/category-984690522.htm?spm=a1z10.5-b.w5001-10132340897.14.u3mA3i&search=y&scene=taobao_shop#TmshopSrchNav"
+            }, {
+                "value": "",
+                "$$hashKey": "object:69",
+                "text": "周边游",
+                "url": "https://cgzl.alitrip.com/category-945317391.htm?spm=a1z10.5-b.w5001-10132340897.15.hliODb&search=y&parentCatId=927118465&parentCatName=%B9%E3%B6%AB%D6%DC%B1%DF%D3%CE&catName=%B8%FA%CD%C5%D3%CE&scene=taobao_shop"
+            }, {
+                "value": "",
+                "$$hashKey": "object:73",
+                "text": "关于我们",
+                "url": "https://cgzl.alitrip.com/p/rd504540.htm?spm=a1z10.5-b.w5001-10132340897.16.RML9mT&scene=taobao_shop"
+            }], "config": {"title": "导航栏", "node-type": "elemtary-reapeat", "max": 10, "controller": "A_2"}
+        },
+        "config": {"title": "店铺招牌", "node_type": "parent", "data_num": 0}
+    };
+
+
+    root.banner = {
+        "banner_pic_repeat": {
+            "model": [{
+                "value": "",
+                "$$hashKey": "object:25",
+                "pic": "https://img.alicdn.com/imgextra/i2/2067413496/TB2OTSIeVXXXXcfXXXXXXXXXXXX_!!2067413496.jpg",
+                "url": "http://cgzl.tmall.com/p/rd743438.htm"
+            }, {
+                "value": "",
+                "$$hashKey": "object:7",
+                "pic": "https://img.alicdn.com/imgextra/i1/2067413496/TB2ikBPdVXXXXXpXXXXXXXXXXXX_!!2067413496.jpg",
+                "url": "http://cgzl.tmall.com/p/rd600857.htm"
+            }, {
+                "value": "",
+                "$$hashKey": "object:55",
+                "pic": "https://img.alicdn.com/imgextra/i2/2067413496/TB2ZzY1dVXXXXcgXXXXXXXXXXXX_!!2067413496.jpg",
+                "url": "http://cgzl.tmall.com/p/rd075928.htm"
+            }, {
+                "value": "",
+                "$$hashKey": "object:54",
+                "pic": "http://img.alicdn.com/imgextra/i2/2067413496/TB2RbfrdFXXXXXtXXXXXXXXXXXX_!!2067413496.jpg",
+                "url": "http://cgzl.alitrip.com/p/rd873746.htm?spm=a1z10.1-b.w5003-9882021204.1.W1lMRr&scene=taobao_shop"
+            }, {
+                "value": "",
+                "$$hashKey": "object:61",
+                "pic": "http://img03.taobaocdn.com/imgextra/i3/2067413496/TB2LTBrbVXXXXatXpXXXXXXXXXX_!!2067413496.jpg",
+                "url": "http://cgzl.tmall.com/p/visa.htm"
+            }],
+            "config": {
+                "title": "焦点图详情",
+                "node-type": "elemtary-reapeat",
+                "controller": "B_1",
+                "max": 7,
+                "picSize-x": "140",
+                "picSize-y": "31"
+            }
+        }, "config": {"title": "焦点图", "node-type": "parent", "data_num": 1}
+    };
+
+
+    root.callcenter = {
+        "travel_repeat": {
+            "config": {
+                "title": "旅游度假",
+                "node-type": "elemtary-reapeat",
+                "max": 10,
+                "controller": "A_3"
+            },
+            "model": [{"value": "", "$$hashKey": "object:7", "text": "坤坤"}, {
+                "value": "",
+                "$$hashKey": "object:44",
+                "text": "大吉"
+            }, {"value": "", "$$hashKey": "object:49", "text": "岚岚"}, {
+                "value": "",
+                "$$hashKey": "object:53",
+                "text": "大斌"
+            }, {"value": "", "$$hashKey": "object:58", "text": "梵梵"}]
+        },
+        "ticket_repeat": {
+            "config": {"title": "订房订票", "node-type": "elemtary-reapeat", "max": 10, "controller": "A_3"},
+            "model": [{"value": "", "$$hashKey": "object:7", "text": "云儿"}, {
+                "value": "",
+                "$$hashKey": "object:63",
+                "text": "珊珊"
+            }, {"value": "", "$$hashKey": "object:68", "text": "斋斋"}]
+        },
+        "config": {"title": "旺旺客服", "node-type": "parent", "data_num": 2}
+    };
+
+    root.catalog = {
+        "catalog_item_repeat": {
+            "model": [{
+                "value": "",
+                "$$hashKey": "object:7",
+                "text": "出境游度假",
+                "sub_item_repeat": [{"text": "美洲", "url": "http://www.baidu.com"}, {
+                    "text": "美洲",
+                    "url": "http://www.baidu.com"
+                }]
+            }]
+
+        },
+        "catalog_banner_repeat": {
+            "model": [{"value": "", "$$hashKey": "object:7", "text": "省内沙滩", "type": "default"}]
+        }
+    };
+
 
     //店铺招牌设置
     root.headerStyle.config = {"title": "店铺招牌", "node_type": "parent", "data_num": 0};
-    $.extend(root.headerStyle.hot_search_repeat.config , {
+    $.extend(root.headerStyle.hot_search_repeat.config, {
         "node_type": "elemtary-reapeat",
         "controller": "A_1"
     });
@@ -37,15 +200,24 @@ dataScope.factory('sData',function(){
 
     //旺旺客服设置
     root.callcenter.config = {"title": "旺旺客服", "node-type": "parent", "data_num": 2};
-    $.extend(root.callcenter.travel_repeat.config , {
+    $.extend(root.callcenter.travel_repeat.config, {
         "node-type": "elemtary-reapeat",
         "max": 10,
         "controller": "A_3"
     });
-    $.extend(root.callcenter.ticket_repeat.config , {
+    $.extend(root.callcenter.ticket_repeat.config, {
         "node-type": "elemtary-reapeat",
         "max": 10,
         "controller": "A_3"
+    });
+
+    root.catalog.config={
+        "title":"分类栏目",
+        "data_num":3
+    };
+    $.extend(root.catalog.catalog_item_repeat.config,{
+        "max": 5,
+        "controller": "A_4"
     });
 
     return root;
